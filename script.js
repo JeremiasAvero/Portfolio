cac50d87101490a9b3fa699ec5f687989f7776f2
JeremiasAvero <jeremiasavero98@gmail.com>
2024-01-09 11:02:50 -0300

    responsive added

--- script.js
+++ script.js
@@ -1,9 +1,32 @@
-document.getElementById("Resume").addEventListener("click", function (event) {
-  event.preventDefault(); // Evita que se abra la página por defecto
+document.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("Resume").addEventListener("click", function (event) {
+    event.preventDefault(); // Evita que se abra la página por defecto
 
-  // Simular un clic en el enlace para iniciar la descarga
-  var link = document.createElement("a");
-  link.href = "JeremiasAvero.pdf"; // Asegúrate de que sea la ruta correcta
-  link.download = "JeremiasAvero-Resume-CV.pdf";
-  link.click();
+    // Simular un clic en el enlace para iniciar la descarga
+    var link = document.createElement("a");
+    link.href = "JeremiasAvero.pdf"; // Asegúrate de que sea la ruta correcta
+    link.download = "JeremiasAvero-Resume-CV.pdf";
+    link.click();
+  });
+
+  let mainContainer = document.getElementById("main");
+  let btn = document.getElementById("nav-res-btn");
+
+  btn.addEventListener("click", () => {
+    // Toggle visibility using a class
+    mainContainer.classList.toggle("hidden");
+  });
 });
+// let mainContainer = document.getElementById("main");
+// let btn = document.getElementById("nav-res-btn");
+// btn.addEventListener("click", () => {
+//   console.log("hola");
+//   if (
+//     mainContainer.style.display === "none" ||
+//     mainContainer.style.display === ""
+//   ) {
+//     mainContainer.style.display = "block"; // or "inline" or any other valid display value
+//   } else {
+//     mainContainer.style.display = "none";
+//   }
+// });
